3bd583cea9215a8fac52b8953bb1b482340c2a1e
Dmitriy <dmitiyoleynik@gmail.com>
2020-07-06 17:02:16 +0300

    Add serilog logger

--- Backend/FrelanceSystem/FrelanceSystem/Startup.cs
+++ Backend/FrelanceSystem/FrelanceSystem/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace FrelanceSystem
 {
@@ -26,6 +20,13 @@ namespace FrelanceSystem
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            ILogger logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build())
+                .CreateLogger();
+            services.AddSingleton<ILogger>(logger);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
