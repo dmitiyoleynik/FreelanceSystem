4e8ef302cdb681678e9c068a2a09baba65de09cf
Igor-Belokon <igorbelokon8@gmail.com>
2020-07-08 21:26:02 +0300

    fixed axios change files

--- front-end/src/store/action/data.js
+++ front-end/src/store/action/data.js
@@ -1,12 +1,8 @@
 import { successAction, failureAction } from "../types";
 import axios from "axios";
 
-export function get() {
-  return dispatch =>
-    axios
-      .get("")
-      .then(data => dispatch({ type: successAction(SAME_DATA), data }))
-      .catch(err => dispatch({ type: failureAction(SAME_DATA), err }));
+const instance = axios.create({
+  baseURL: ""
+});
 
-}
 
--- front-end/src/store/types.js
+++ front-end/src/store/types.js
@@ -1,9 +1,4 @@
 export const successAction = type => `${type}_SUCCESS`;
 export const failureAction = type => `${type}_FAILED`;
 
-export const NEWS = "NEWS";
-
-
-export const ItemTypes = {
-  DATA: "data"
-};
+export const ITEM_TYPES = {};
